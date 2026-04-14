ad67141eb87984e11602e8ead7d0097b3e43df0c
Aviral <uncleprosk@gmail.com>
2026-04-14 22:42:17 +0530

    added 2048 game

---
+++ events/2048.js
@@ -0,0 +1,91 @@
+const { Client, Events, Interaction, MessageFlags, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, PermissionsBitField, ModalBuilder, TextInputBuilder, TextInputStyle} = require('discord.js');
+
+/**
+ * 
+ * @param {Client} client 
+ * @param {Interaction} interaction 
+ */
+module.exports = {
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        const client = interaction.client;
+    
+        try {
+            if (!interaction.inGuild()) return;
+            if (!interaction.isModalSubmit() && !interaction.isButton()) return;
+
+            const key = interaction.message.id;
+
+            let playersData = await client.cache.get(key); //below given format
+            let players = []; //array of users only
+
+            if (playersData?.length > 0) {
+                for (const playerData of playersData) {
+                    players.push(playerData.user);
+                }
+            }
+
+            let playerData = playersData?.find(user => user.user === interaction.member.user);
+
+            /**
+             *  format of 2048 cache
+             *      {
+             *           user,
+             *           matrix,
+             *           turns: 0,
+             *           ended: false,
+             *           next: {
+             *               up: true,
+             *               down: true,
+             *               left: true,
+             *               right: true,
+             *           }
+             */
+
+            // 2048 play button
+            if (interaction.customId === 'play2048' && interaction.isButton()) {
+                console.log("ok");
+                let matrix2048 = playerData?.matrix;
+                let ended2048 = playerData?.ended;
+                let turns2048 = playerData?.turns;
+                let next2048 = playerData?.next;
+
+                const up2048 = new ButtonBuilder()
+                    .setCustomId("up2048")
+                    .setLabel("⬆️")
+                    .setStyle(ButtonStyle.Primary);
+                const down2048 = new ButtonBuilder()
+                    .setCustomId("down2048")
+                    .setLabel("⬇️")
+                    .setStyle(ButtonStyle.Primary);
+                const left2048 = new ButtonBuilder()
+                    .setCustomId("left2048")
+                    .setLabel("⬅️")
+                    .setStyle(ButtonStyle.Primary);
+                const right2048 = new ButtonBuilder()
+                    .setCustomId("right2048")
+                    .setLabel("➡️")
+                    .setStyle(ButtonStyle.Primary);
+                
+                const row2048 = new ActionRowBuilder()
+                    .addComponents(up2048, down2048, left2048, right2048);
+
+                //if (players.includes(interaction.member.user) && !ended2048) {
+                    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+                    const Msg2048 = await interaction.editReply({
+                        content: `${matrix2048}`,
+                        components: [row2048],
+                    });
+                    console.log(matrix2048);
+                //}
+            }
+            // MODALS HANDLER
+
+            if (interaction.isModalSubmit() && players.includes(interaction.member.user)) {
+
+            } 
+        } catch (error) {
+            console.log(`Error while hangman game: ${error}`);
+        }
+    }
+};
--- events/buttonAndModal.js
+++ events/buttonAndModal.js
@@ -44,19 +44,6 @@ module.exports = {
              *        shots: 0,
              *        died: false,
              *      }
-             * 
-             *  format of 2048 cache
-             *      {
-             *           user,
-             *           matrix,
-             *           turns: 0,
-             *           ended: false,
-             *           next: {
-             *               up: true,
-             *               down: true,
-             *               left: true,
-             *               right: true,
-             *           }
              */
 
 
@@ -135,43 +122,7 @@ module.exports = {
                 }
             }
 
-            // 2048 play button
-            if (interaction.customId === 'play2048' && interaction.isButton()) {
-                console.log("ok");
-                let matrix2048 = playerData?.matrix;
-                let ended2048 = playerData?.ended;
-                let turns2048 = playerData?.turns;
-                let next2048 = playerData?.next;
-
-                const up2048 = new ButtonBuilder()
-                    .setCustomId("up2048")
-                    .setLabel("⬆️")
-                    .setStyle(ButtonStyle.Primary);
-                const down2048 = new ButtonBuilder()
-                    .setCustomId("down2048")
-                    .setLabel("⬇️")
-                    .setStyle(ButtonStyle.Primary);
-                const left2048 = new ButtonBuilder()
-                    .setCustomId("left2048")
-                    .setLabel("⬅️")
-                    .setStyle(ButtonStyle.Primary);
-                const right2048 = new ButtonBuilder()
-                    .setCustomId("right2048")
-                    .setLabel("➡️")
-                    .setStyle(ButtonStyle.Primary);
-                
-                const row2048 = new ActionRowBuilder()
-                    .addComponents(up2048, down2048, left2048, right2048);
-
-                //if (players.includes(interaction.member.user) && !ended2048) {
-                    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-                    const Msg2048 = await interaction.editReply({
-                        content: `${matrix2048}`,
-                        components: [row2048],
-                    });
-                    console.log(matrix2048);
-                //}
-            }
+            
             // MODALS HANDLER
 
             if (interaction.isModalSubmit() && players.includes(interaction.member.user)) {
